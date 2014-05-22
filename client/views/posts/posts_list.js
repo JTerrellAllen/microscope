ebf7437582d7c63f32965605c2421d417448ed6f
JTerrellAllen <jterrellallen@gmail.com>
2014-05-22 01:42:15 -0500

    4-3 Wired collection into  template.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/'
-  },
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://sachagreif.com/'
-  },
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://sachagreif.com/'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
